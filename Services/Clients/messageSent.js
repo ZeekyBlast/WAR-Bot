c34e053ad8550964bdc04a36770fb04fdc9f24fb
ZeekyBlast <benroram@gmail.com>
2026-04-24 19:41:32 -0700

    added cooldowns to xp gain

--- Services/Clients/messageSent.js
+++ Services/Clients/messageSent.js
@@ -1,9 +1,11 @@
 const { loadCMDS } = require('../../Handlers/commandHandler')
 const userService = require('../DB/UserService')
-const { PresenceUpdateStatus, ActivityType } = require('discord.js')
+const { PresenceUpdateStatus, ActivityType, Collection } = require('discord.js')
 const { ChatInputCommandInteraction, EmbedBuilder, GuildMember, Message } = require('discord.js')
 const { addXPAndCheckLevel } = require('../DB/levelService')
 const { calcRequiredXP } = require('../DB/mathService')
+
+let cooldowns = new Collection()
 
 module.exports = {
     name: 'messageCreate', // name
@@ -16,6 +18,22 @@ module.exports = {
     async execute(message, interaction) { //anything in the () is something that isn't basic js so if you need the client which is the bot you put it in there
 
         if(message.author.bot) return;
+
+        const cooldownAmount = 60000
+        const now = Date.now()
+
+        if (cooldowns.has(message.author.id)){
+            const expiration = cooldowns.get(message.author.id) + cooldownAmount
+
+            if (now < expiration){
+                const timeLeft = (expiration - now) / 1000
+                return;
+            }
+        }
+
+        cooldowns.set(message.author.id, now)
+
+        setTimeout(() => cooldowns.delete(message.author.id), cooldownAmount)
 
         const userId = message.author.id;
 
